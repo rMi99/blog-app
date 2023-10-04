fc810fafa02097e0cf705ce616e3717399d75629
rMi99 <99ramigimhama@gmail.com>
2023-10-05 01:17:00 +0530

    add post policy

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\cr;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 
 class PostController extends Controller
 {
@@ -12,54 +14,59 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        /// For pagination
+$posts = Post::paginate(10);
+
+// For searching
+$search = request('search');
+$posts = Post::where('title', 'like', "%$search%")
+    ->orWhere('content', 'like', "%$search%")
+    ->paginate(10);
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+
+
+public function store(Request $request)
+{
+    $validator = Validator::make($request->all(), [
+        'title' => 'required|max:255',
+        'content' => 'required',
+        'image' => 'nullable|image|max:1024', // 1MB limit
+    ]);
+
+    if ($validator->fails()) {
+        return redirect()->route('posts.create')
+            ->withErrors($validator)
+            ->withInput();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+    // Create and save the post here
+
+    return redirect()->route('posts.index')
+        ->with('success', 'Post created successfully.');
+}
+
+public function update(Request $request, Post $post)
+{
+    $validator = Validator::make($request->all(), [
+        'title' => 'required|max:255',
+        'content' => 'required',
+        'image' => 'nullable|image|max:1024', // 1MB limit
+    ]);
+
+    if ($validator->fails()) {
+        return redirect()->route('posts.edit', $post->id)
+            ->withErrors($validator)
+            ->withInput();
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(cr $cr)
-    {
-        //
-    }
+    // Update the post here
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(cr $cr)
-    {
-        //
-    }
+    return redirect()->route('posts.index')
+        ->with('success', 'Post updated successfully.');
+}
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, cr $cr)
-    {
-        //
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(cr $cr)
-    {
-        //
-    }
+   
 }
